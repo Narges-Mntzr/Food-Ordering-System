8616c9bf0b9bd232b671f24552945cd741b6a98e
Parto <79199073+sabzparto@users.noreply.github.com>
2024-05-17 22:27:59 +0330

    Add files via upload

---
+++ Files/restaurant.sql
@@ -0,0 +1,10 @@
+create table restaurant (
+name varchar(20) not null,
+score int null,
+sales_number int null,
+create_time datetime null,
+shipping_cost int null,
+respone_time int null,
+address varchar(50),
+primary key(name)
+)
