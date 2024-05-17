38da46d6652d8e0af3d09918054b3253bd39b3ed
Parto <79199073+sabzparto@users.noreply.github.com>
2024-05-17 22:58:45 +0330

    Update discount.sql

--- Files/discount.sql
+++ Files/discount.sql
@@ -2,14 +2,13 @@ create table discount (
 discount_code int not null,
 amount int null,
 type varchar(10) null check ( status in ('percent', 'cash')),
-status  varchar(10) null check ( status in ('used', 'not used')),
 expire_time datetime null,
 primary key(discount_code)
 );
 
 INSERT INTO discount 
-    (discount_code, amount, type, status, expire_time)
+    (discount_code, amount, type, expire_time)
 VALUES
-    (1, 10, 'percent', 'not used', '2024-11-09 15:45:21'),
-    (2, 20, 'percent', 'not used', '2024-9-09 22:10:21'),
-    (3, 30000, 'cash', 'not used', '2024-10-09 23:45:21')
+    (1, 10, 'percent', '2024-11-09 15:45:21'),
+    (2, 20, 'percent', '2024-9-09 22:10:21'),
+    (3, 30000, 'cash', '2024-10-09 23:45:21')
